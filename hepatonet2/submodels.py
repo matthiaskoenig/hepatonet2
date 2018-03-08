dfc731b5e3d855dcacb17ea17ac9572ffd6bdc08
Matthias Koenig <konigmatt@googlemail.com>
2018-03-08 23:37:33 +0100

    updated gene mapping

--- hepatonet2/submodels.py
+++ hepatonet2/submodels.py
@@ -5,6 +5,8 @@ import os
 import json
 from collections import defaultdict
 
+import libsbml
+
 dir_cur = os.path.dirname(os.path.realpath(__file__))
 repo_dir = os.path.join(dir_cur, "..", "repository")
 
@@ -19,6 +21,25 @@ def parse_submodels():
 
         with open(os.path.join(repo_dir, "subsystems.json"), "w") as f_subsystems:
             json.dump(subsystems, f_subsystems, sort_keys=True, indent=2)
+
+
+def create_sbml_submodels(subsystem, organism):
+    doc = libsbml.SBMLDocument(3, 1)  # type: libsbml.SBMLDocument
+    model = doc.createModel()
+    mid = subsystem.replace(" ", '_')
+    mid = mid.replace("-", "_")
+
+    model.setId(mid)
+
+    with open(os.path.join(repo_dir, "subsystems.json"), "r") as f_reactions:
+        reactions = json.load(f_reactions)
+
+        for rid, r in reactions.items():
+            if r.get("subsystems") == subsystem:
+
+            # add reactions which belong to the subsystem
+            
+
 
 if __name__ == "__main__":
     parse_submodels()
