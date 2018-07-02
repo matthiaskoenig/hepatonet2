d139855ff6fa1ea012d5fe37f147b91dcd86c28d
Matthias Koenig <konigmatt@googlemail.com>
2018-07-02 11:35:41 +0200

    working on subsystem extraction

--- hepatonet2/submodels.py
+++ hepatonet2/submodels.py
@@ -10,9 +10,14 @@ import libsbml
 dir_cur = os.path.dirname(os.path.realpath(__file__))
 repo_dir = os.path.join(dir_cur, "..", "repository")
 models_dir = os.path.join(dir_cur, "..", "models")
+input_dir = os.path.join(dir_cur, "..", "input")
+
 
 def parse_submodels():
+    """ Parses the subsystem information.
 
+    :return:
+    """
     subsystems = defaultdict(list)
     with open(os.path.join(repo_dir, "reactions.json"), "r") as f_reactions:
         reactions = json.load(f_reactions)
@@ -22,7 +27,7 @@ def parse_submodels():
 
         with open(os.path.join(repo_dir, "subsystems.json"), "w") as f_subsystems:
             json.dump(subsystems, f_subsystems, sort_keys=True, indent=2)
-
+    return subsystems
 
 def _normalize_to_sid(text):
     """ Process text to be SID conform. """
@@ -41,10 +46,78 @@ def create_sbml_for_subsystem(subsystem, organism):
     :param organism:
     :return:
     """
+    # load RECON3D reference
+    path_recon = os.path.join(input_dir, "models", "recon3d", "Recon3D.xml")
+    print(path_recon)
+    doc_recon = libsbml.readSBMLFromFile(path_recon)  # type: libsbml.SBMLDocument
+    model_recon = doc_recon.getModel()  # type: libsbml.Model
+
+
     doc = libsbml.SBMLDocument(3, 2)  # type: libsbml.SBMLDocument
     # TODO: add fbc package
-    model = doc.createModel()
+    model = doc.createModel()  # type: libsbml.Model
     model.setId(_normalize_to_sid(subsystem))
+
+    # reaction ids for subsystem
+    reaction_ids = set()
+    compartment_ids = set()
+    species_ids = set()
+    geneproduct_ids = set()
+
+    with open(os.path.join(repo_dir, "reactions.json"), "r") as f_reactions:
+        reactions = json.load(f_reactions)
+
+        for rid, rinfo in reactions.items():
+            if rinfo.get("subsystems") == subsystem:
+                reaction_ids.add(rid)
+                r_recon = model_recon.getReaction(rid)  # type: libsbml.Reaction
+                for s in r_recon.getListOfReactants():  # type: libsbml.SpeciesReference
+                    species_ids.add(s.getSpecies())
+                for s in r_recon.getListOfProducts():
+                    species_ids.add(s.getSpecies())
+                for s in r_recon.getListOfModifiers():
+                    species_ids.add(s.getSpecies())
+
+                # parse the geneProduct information
+
+    # compartments
+    for sid in species_ids:
+        c_recon = model_recon.getSpecies(sid)
+        compartment_ids.add(c_recon.getId())
+
+    # add compartments in model
+    for cid in compartment_ids:
+        c_recon = model_recon.getCompartment(cid)  # type: libsbml.Compartment
+        c = model.createCompartment()  # type: libsbml.Compartment
+        c.setId(c_recon.getId())
+        c.setConstant(c_recon.getConstant())
+        c.setMetaId(c_recon.getMetaId())
+        c.setName(c_recon.getName())
+        c.setSBOTerm(c_recon.getSBOTerm())
+        # c.setUnits(c_recon.getUnits())  # FIXME (no units so far)
+        c.setAnnotation(c_recon.getAnnotation())
+
+    # add species in model
+    for sid in species_ids:
+        s_recon = model_recon.getSpecies(sid)  # type: libsbml.Species
+        s = model.createSpecies()  # type: libsbml.Species
+        s.setId(s_recon.getId())
+        s.setBoundaryCondition(s_recon.getBoundaryCondition())
+        s.setCompartment(s_recon.getCompartment())
+        s.setConstant(s_recon.getConstant())
+        s.setHasOnlySubstanceUnits(s_recon.getHasOnlySubstanceUnits())
+        s.setName(s_recon.getName())
+
+        # fbc
+        s_recon_fbc = s_recon.getPlugin("fbc")  # type: libsbml.FbcSpeciesPlugin
+        s_fbc = s.getPlugin("fbc")  # type: libsbml.FbcSpeciesPlugin
+        s_fbc.setCharge(s_recon_fbc.getCharge())
+        s_fbc.setChemicalFormula(s_recon_fbc.getChemicalFormula())
+
+
+
+
+
 
     with open(os.path.join(repo_dir, "reactions.json"), "r") as f_reactions, \
          open(os.path.join(repo_dir, "species.json"), "r") as f_species, \
@@ -76,8 +149,7 @@ def create_sbml_for_subsystem(subsystem, organism):
                     specie = species[species_id]
                     compartment_id = specie['compartment']
 
-
-                for stoichiometry, species_id in rinfo['rxn_left']:
+                for stoichiometry, species_id in rinfo['rxn_right']:
                     print(stoichiometry, species_id)
 
                 # add compartments for the species
@@ -86,14 +158,18 @@ def create_sbml_for_subsystem(subsystem, organism):
 
                 # add gene associations for the subsystem
 
+                # add annotations
+
+
     return doc
 
 
 if __name__ == "__main__":
-    parse_submodels()
+    subsystems_dict = parse_submodels()
 
     # Create submodels
     subsystems = ['Glycolysis/gluconeogenesis']
+    # subsystems = subsystems_dict.keys()
     organism = "human"
     for subsystem in subsystems:
         doc = create_sbml_for_subsystem(subsystem=subsystem, organism=organism)
